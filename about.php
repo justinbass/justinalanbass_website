1f3e9609ba315356b51dae701af59da2baaee818
root <root@raspberrypi>
2015-12-17 14:24:03 -0800

    Resume TeX, HTTPS disabled

--- about.php
+++ about.php
@@ -5,7 +5,7 @@ $detect = new Mobile_Detect();
 $body = 
 "
 <h1>About</h1>
-<p>This website is set up using a Raspberry Pi running Apache 2.0 and using HTTPS.</p>
+<p>This website is set up using a Raspberry Pi 2 running Apache 2.0 and using HTTP. It is sometimes port-forwarded through port 8080, depending on which ISP I am using.</p>
 ";
 
 if ($detect->isMobile()) {
